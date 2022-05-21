c247c59996330d52365c024c43116fb3af761933
DENIS BLINOV <blinovda1@mail.ru>
2022-05-21 16:10:46 +0300

    add task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -5,3 +5,28 @@
 // 23432 -> да
 // 12821 -> да
 
+Console.Clear();
+Console.Write("Введите число: ");
+
+string s_n_1 = Console.ReadLine();
+int n_1 = int.Parse(s_n_1);
+
+void Palindrome(int n)
+{
+    int i=0;
+    string num = n.ToString();
+    int size = num.Length -1;
+    string flag = "yes";
+
+    while (i<size /2)
+    {
+        if (num[i] != num[size - i])
+        {
+            flag = "no";
+            break;
+        }
+        i += 1;
+    }
+    Console.WriteLine($"{n} -> {flag}");
+}
+Palindrome(n_1);
